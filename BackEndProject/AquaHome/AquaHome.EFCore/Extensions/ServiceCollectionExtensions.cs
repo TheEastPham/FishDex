8e86ff743a3bb657a72cbc37b0d6e87595cecab1
DonPham <donpham@gmail.com>
2026-06-08 20:55:27 +0700

    remove autofac

---
+++ BackEndProject/AquaHome/AquaHome.EFCore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using AquaHome.EFCore.Repository;
+using AquaHome.EFCore.Repository.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AquaHome.EFCore.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAquaHomeRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAquariumRepository, AquariumRepository>();
+        services.AddScoped<IUserFavoriteRepository, UserFavoriteRepository>();
+        return services;
+    }
+}
---
+++ BackEndProject/FishDex/FishDex.Domain/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using FishDex.Domain.Services;
+using FishDex.Domain.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FishDex.Domain.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddFishDexServices(this IServiceCollection services)
+    {
+        services.AddScoped<ISpeciesService, SpeciesService>();
+        services.AddScoped<IEcologyService, EcologyService>();
+        services.AddScoped<IStockService, StockService>();
+        services.AddScoped<IMorphDataService, MorphDataService>();
+        services.AddScoped<IEcosystemService, EcosystemService>();
+        services.AddScoped<IOccurrenceService, OccurrenceService>();
+        services.AddScoped<IMediaService, MediaService>();
+        services.AddSingleton<IStorageService, S3StorageService>();
+        return services;
+    }
+}
